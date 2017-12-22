1bf03022a69fcbbbbc2cc450601b0f829ddecb53
Jim Keller <jimk@easternstandard.com>
2017-12-22 00:00:06 -0500

    updated getter for options so that nested option keys dont get lost

--- accordion.js
+++ accordion.js
@@ -1,61 +1,37 @@
 var Accordion = function ( options ) {
 
-  //
-  // Assign defaults
-  //
-  this.options = {
-    'selector_bundle': '.accordion', //selector for outer container for multiple accordion items (an accordion item consists of heading and content, a "bundle" consists of multiple items)
-    'selector_item': '.accordion__item', //selector for single accordion item (heading + content)
-    'selector_heading': '.accordion__item__heading a', //selector for clickable accordion heading
-    'selector_content': '.accordion__item__content', //selector for clickable accordion content
-    'id_prefix_content': 'accordion-content',
-    'id_prefix_bundle': 'accordion-bundle',
-    'selector_tabbable_elements': 'input, a', //selector for elements that can be tabbed to. These are disabled in the accordion content when the accordion is closed
-    'scroll_to_top': true,
-    'selector_scroll_element': 'window', //What container to scroll when accordion opens
-    'class_name_expanded': 'expanded', //Class name for 'expanded' accordion
-    'auto_expand_first_item': false, //If you want the first item to default to expanded
-    'item_expand_unique': false, //If true, other items will be closed when an item is expanded
-    'height_buffer': 0, //If you want extra padding below the content when the accordion is expanded
-    'height_units': 'px',
-    'callbacks': {
-      //
-      // Open and close callbacks are passed the following object as a parameter:
-      // {
-      //  item: [the item that was opened or closed, as a dom object]
-      //  bundle: [the bundle containing this item, as a dom object]
-      //  accordion_object: [this object]
-      // }
-      'open': {
-        'before': null, //Callback to fire before an accordion is opened (but after it is clicked/tapped)
-        'after': null //Callback to fire after an accordion is opened
-      },
-      'close': {
-        'before': null, //Callback to fire before an accordion is closed (but after it is clicked/tapped)
-        'after': null //Callback to fire after an accordion is closed
-      },
-      'item': {
-        'initialize': {
-          'after': null //Callback fired after an accordion item is initialized
-        }
+  this.options = options;
+  this.initialize();
+
+}
+
+/**
+ * Gets an option by name, or sets an option if val parameter is present. Checks local options and falls back to global options
+ * @param {string} key
+ * @param {mixed} val (optional)
+ * @return none
+ */
+Accordion.prototype.option = function ( key, val ) {
+  try {
+
+    var ret;
+
+    if ( typeof(val) === 'undefined' ) {
+      ret = Accordion.obj_get_if_set( this.options, key);
+
+      if ( typeof(ret) == 'undefined' ) {
+        ret = Accordion.obj_get_if_set( Accordion.options_default(), key );
       }
 
-    },
-    'css_style_apply': false, //EXPERIMENTAL if you want to apply all your own CSS, set this to false. These defaults are probably necessary though
-    'css_styles': { //if css_style_apply is true, these styles will be applied
-      'collapsed': {
-        'content': { 'height': '0px'  }
-      },
-      'expanded': {}
+      return ret;
     }
-
-  };
-
-  for ( var key in options ) {
-    this.options[key] = options[key];
+    else {
+      this.options[key] = val;
+    }
   }
-
-  this.initialize();
+  catch(e) {
+    throw e;
+  }
 
 }
 
@@ -84,13 +60,13 @@ Accordion.prototype.resize_handler = function( options ) {
 
     try {
 
-      var bundles = document.querySelectorAll(this.options.selector_bundle);
+      var bundles = document.querySelectorAll(this.option('selector_bundle'));
       options = options || {};
 
       for ( var bundle_index = 0; bundle_index < bundles.length; bundle_index++ ) {
 
         var bundle = bundles[bundle_index];
-        var items = bundle.querySelectorAll(this.options.selector_item);
+        var items = bundle.querySelectorAll(this.option('selector_item'));
 
         for( var i = 0; i < items.length; i++ ) {
 
@@ -108,29 +84,29 @@ Accordion.prototype.resize_handler = function( options ) {
 
 Accordion.prototype.initialize = function() {
 
-  if ( typeof(this.options.selector_bundle) == 'undefined' || this.options.selector_bundle == '' ) {
+  if ( typeof(this.option('selector_bundle')) == 'undefined' || this.option('selector_bundle') == '' ) {
     throw 'No bundle selector specified';
     return false;
   }
 
-  var bundles = document.querySelectorAll(this.options.selector_bundle);
+  var bundles = document.querySelectorAll(this.option('selector_bundle'));
 
   for ( var bundle_index = 0; bundle_index < bundles.length; bundle_index++ ) {
 
     var bundle = bundles[bundle_index];
-    var items = bundle.querySelectorAll(this.options.selector_item);
+    var items = bundle.querySelectorAll(this.option('selector_item'));
     var me = this;
 
     bundle.setAttribute('role', 'tablist');
-    bundle.setAttribute('id', this.options.id_prefix_bundle + '-' + bundle_index.toString() );
+    bundle.setAttribute('id', this.option('id_prefix_bundle') + '-' + bundle_index.toString() );
 
     for( var item_index = 0; item_index < items.length; item_index++ ) {
       this.item_initialize(items[item_index], item_index, bundle, bundle_index);
     }
 
-    this.bundle_collapse( bundle, { first_initialize: true, except_first: this.options.auto_expand_first_item });
+    this.bundle_collapse( bundle, { first_initialize: true, except_first: this.option('auto_expand_first_item') });
 
-    if ( Accordion.obj_get_if_set(this.options, 'auto_expand_first_item') == true ) {
+    if ( this.option('auto_expand_first_item') == true ) {
       this.item_expand(items[0]);
     }
   }
@@ -163,18 +139,18 @@ Accordion.prototype.item_initialize = function( item, item_index, bundle, bundle
   try {
     var me = this;
     var callback;
-    var heading = item.querySelector(this.options.selector_heading);
-    var content = item.querySelector(this.options.selector_content);
+    var heading = item.querySelector(this.option('selector_heading'));
+    var content = item.querySelector(this.option('selector_content'));
     var unique_suffix = bundle_index.toString() + '_' + item_index.toString();
-    var item_unique_id = this.options.selector_bundle.replace(/[^A-Za-z0-9-_]*/g, '') + '_' + unique_suffix;
+    var item_unique_id = this.option('selector_bundle').replace(/[^A-Za-z0-9-_]*/g, '') + '_' + unique_suffix;
 
     item.setAttribute('data-accordion-item-id', item_unique_id );
     item.setAttribute('data-accordion-bundle-id', bundle.getAttribute('id') );
     heading.setAttribute('data-accordion-header-for', item_unique_id);
     heading.setAttribute('role', 'tab');
-    heading.setAttribute('aria-controls', '#' + me.options.id_prefix_content + '-' + unique_suffix );
+    heading.setAttribute('aria-controls', '#' + me.option('id_prefix_content') + '-' + unique_suffix );
 
-    content.setAttribute('id', me.options.id_prefix_content + '-' + unique_suffix)
+    content.setAttribute('id', me.option('id_prefix_content') + '-' + unique_suffix)
     content.setAttribute('role', 'tabpanel');
 
     heading.addEventListener('click',
@@ -190,23 +166,23 @@ Accordion.prototype.item_initialize = function( item, item_index, bundle, bundle
           event.preventDefault();
           event.stopPropagation();
 
-          var click_item = document.querySelector( me.options.selector_item + '[data-accordion-item-id=' + click_item_id + ']' );
+          var click_item = document.querySelector( me.option('selector_item') + '[data-accordion-item-id=' + click_item_id + ']' );
 
           if ( !me.item_is_expanded(click_item) ) {
 
             //
             // Fire 'before' open callback
             //
-            if ( callback = Accordion.obj_get_if_set(me.options, 'callbacks.open.before')  ) {
+            if ( callback = me.option('callbacks.open.before')  ) {
               callback.call(event, callback_params);
             }
 
-            me.item_expand(click_item, { from_click: true, scroll_to_top: me.options.scroll_to_top} );
+            me.item_expand(click_item, { from_click: true, scroll_to_top: me.option('scroll_to_top')} );
 
             //
             // Fire 'after' open callback
             //
-            if ( callback = Accordion.obj_get_if_set(me.options, 'callbacks.open.after')  ) {
+            if ( callback = me.option('callbacks.open.after')  ) {
               callback.call(event, callback_params);
             }
 
@@ -216,7 +192,7 @@ Accordion.prototype.item_initialize = function( item, item_index, bundle, bundle
             //
             // Fire 'before' close callback
             //
-            if ( callback = Accordion.obj_get_if_set(me.options, 'callbacks.close.before') ) {
+            if ( callback = me.option('callbacks.close.before') ) {
               callback.call(event, callback_params);
             }
 
@@ -225,7 +201,7 @@ Accordion.prototype.item_initialize = function( item, item_index, bundle, bundle
             //
             // Fire 'after' close callback
             //
-            if ( callback = Accordion.obj_get_if_set(me.options, 'callbacks.close.after')  ) {
+            if ( callback = me.option('callbacks.close.after')  ) {
               callback.call(event, callback_params);
             }
           }
@@ -237,7 +213,7 @@ Accordion.prototype.item_initialize = function( item, item_index, bundle, bundle
 
     this.item_expanded_height_calculate(item );
 
-    if ( callback = Accordion.obj_get_if_set(this.options, 'callbacks.item.initialize.after')  ) {
+    if ( callback = this.option('callbacks.item.initialize.after')  ) {
       callback.call(event, callback_params);
     }
 
@@ -261,7 +237,7 @@ Accordion.prototype.tabbables_toggle = function( element, action, options ) {
   try {
     options = options || {};
 
-    var tabbables = element.querySelectorAll(this.options.selector_tabbable_elements);
+    var tabbables = element.querySelectorAll(this.option('selector_tabbable_elements'));
     var tabindex = ( action == 'disable' ) ? '-1' : '0';
 
     for ( var j = 0; j < tabbables.length; j++ ) {
@@ -293,7 +269,7 @@ Accordion.prototype.item_expanded_height_recalculate = function(item, options) {
     var me = this;
 
     var was_expanded = null;
-    var content = item.querySelector(this.options.selector_content);
+    var content = item.querySelector(this.option('selector_content'));
 
     //
     // We need the item to be expanded in order to do this calculation
@@ -330,12 +306,12 @@ Accordion.prototype.item_expanded_height_calculate = function(item, options) {
     try {
       options = options || {};
       var new_height = null;
-      var content = item.querySelector(this.options.selector_content);
+      var content = item.querySelector(this.option('selector_content'));
 
       content.style.removeProperty('height');
       new_height = content.scrollHeight;
 
-      content.setAttribute('data-expanded-height', parseInt(new_height) + parseInt(this.options.height_buffer) );
+      content.setAttribute('data-expanded-height', parseInt(new_height) + parseInt(this.option('height_buffer')) );
 
     }
     catch(e) {
@@ -358,7 +334,7 @@ Accordion.prototype.bundle_collapse = function( bundle, options ) {
     options = options || {};
 
     var collapse = true;
-    var items = bundle.querySelectorAll(this.options.selector_item);
+    var items = bundle.querySelectorAll(this.option('selector_item'));
     var first_item = items[0];
     var item;
 
@@ -390,26 +366,26 @@ Accordion.prototype.bundle_collapse = function( bundle, options ) {
 }
 
 Accordion.prototype.height_apply_collapsed = function(item, options) {
-  var content = item.querySelector( this.options.selector_content );
-  content.style.height = '0' + this.options.height_units;
+  var content = item.querySelector( this.option('selector_content') );
+  content.style.height = '0' + this.option('height_units');
 
 }
 
 Accordion.prototype.height_apply_expanded = function(item, options) {
 
-  var content = item.querySelector( this.options.selector_content );
-  content.style.height = content.getAttribute('data-expanded-height') + this.options.height_units;
+  var content = item.querySelector( this.option('selector_content') );
+  content.style.height = content.getAttribute('data-expanded-height') + this.option('height_units');
 
 }
 
 Accordion.prototype.active_attributes_apply = function( item ) {
   try {
 
-    var heading = item.querySelector( this.options.selector_heading );
-    var content = item.querySelector( this.options.selector_content );
+    var heading = item.querySelector( this.option('selector_heading') );
+    var content = item.querySelector( this.option('selector_content') );
 
     item.setAttribute('data-accordion-expanded', true);
-    item.classList.add( this.options.class_name_expanded );
+    item.classList.add( this.option('class_name_expanded') );
 
     // if ( this.options.css_style_apply ) {
     //   this.css_style_apply(item, { 'expanded': true });
@@ -455,10 +431,10 @@ Accordion.prototype.item_collapse = function( item ) {
 
   try {
 
-    var heading = item.querySelector( this.options.selector_heading );
-    var content = item.querySelector( this.options.selector_content );
+    var heading = item.querySelector( this.option('selector_heading') );
+    var content = item.querySelector( this.option('selector_content') );
 
-    item.classList.remove( this.options.class_name_expanded );
+    item.classList.remove( this.option('class_name_expanded') );
     item.setAttribute('data-accordion-expanded', false);
 
     // if ( this.options.css_style_apply ) {
@@ -498,24 +474,24 @@ Accordion.prototype.item_expand = function( item, options ) {
 
     options = options || {};
 
-    var content = item.querySelector( this.options.selector_content );
+    var content = item.querySelector( this.option('selector_content') );
 
     this.active_attributes_apply(item);
     this.height_apply_expanded(item);
     //$content.slideDown( 400 );
 
     if ( typeof(options.shadow_expand) == 'undefined' || options.shadow_expand == false ) {
-      if ( typeof(this.options.item_expand_unique) !== 'undefined' && this.options.item_expand_unique == true ) {
+      if ( typeof(this.option('item_expand_unique')) !== 'undefined' && this.option('item_expand_unique') == true ) {
         this.bundle_collapse( this.bundle_by_item(item), { except_item: item } );
       }
 
       if ( typeof(options.scroll_to_top) !='undefined' && options.scroll_to_top == true) {
 
-        if ( this.options.selector_scroll_element == 'window' ) {
+        if ( this.option('selector_scroll_element') == 'window' ) {
           window.scrollTo( 0, item.offsetTop );
         }
         else {
-          document.querySelector(this.options.selector_scroll_element).scrollTop = item.offsetTop;
+          document.querySelector(this.option('selector_scroll_element')).scrollTop = item.offsetTop;
         }
       }
     }
@@ -545,4 +521,53 @@ Accordion.obj_get_if_set = function( obj, keys ) {
 
 if ( typeof module !== 'undefined' && module.exports ) {
   module.exports = Accordion;
+}
+
+Accordion.options_default = function() {
+  return {
+    'selector_bundle': '.accordion', //selector for outer container for multiple accordion items (an accordion item consists of heading and content, a "bundle" consists of multiple items)
+    'selector_item': '.accordion__item', //selector for single accordion item (heading + content)
+    'selector_heading': '.accordion__item__heading a', //selector for clickable accordion heading
+    'selector_content': '.accordion__item__content', //selector for clickable accordion content
+    'id_prefix_content': 'accordion-content',
+    'id_prefix_bundle': 'accordion-bundle',
+    'selector_tabbable_elements': 'input, a', //selector for elements that can be tabbed to. These are disabled in the accordion content when the accordion is closed
+    'scroll_to_top': true,
+    'selector_scroll_element': 'window', //What container to scroll when accordion opens
+    'class_name_expanded': 'expanded', //Class name for 'expanded' accordion
+    'auto_expand_first_item': false, //If you want the first item to default to expanded
+    'item_expand_unique': false, //If true, other items will be closed when an item is expanded
+    'height_buffer': 0, //If you want extra padding below the content when the accordion is expanded
+    'height_units': 'px',
+    'callbacks': {
+      //
+      // Open and close callbacks are passed the following object as a parameter:
+      // {
+      //  item: [the item that was opened or closed, as a dom object]
+      //  bundle: [the bundle containing this item, as a dom object]
+      //  accordion_object: [this object]
+      // }
+      'open': {
+        'before': null, //Callback to fire before an accordion is opened (but after it is clicked/tapped)
+        'after': null //Callback to fire after an accordion is opened
+      },
+      'close': {
+        'before': null, //Callback to fire before an accordion is closed (but after it is clicked/tapped)
+        'after': null //Callback to fire after an accordion is closed
+      },
+      'item': {
+        'initialize': {
+          'after': null //Callback fired after an accordion item is initialized
+        }
+      }
+
+    },
+    'css_style_apply': false, //EXPERIMENTAL if you want to apply all your own CSS, set this to false. These defaults are probably necessary though
+    'css_styles': { //if css_style_apply is true, these styles will be applied
+      'collapsed': {
+        'content': { 'height': '0px'  }
+      },
+      'expanded': {}
+    }
+  }
 }
